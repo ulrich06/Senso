e99ccb9ac3481e818b628002bb9db702d6e20727
Cyril Cecchinel <ulrich06@users.noreply.github.com>
2016-07-25 10:34:49 +0200

    store timestamps in eprom

--- senso_eeprom.h
+++ senso_eeprom.h
@@ -2,18 +2,23 @@
 #define EEPROM_SIZE 512
 
 const int ADD_BUFFER = 0;
-const int ADD_SYNC = ADD_BUFFER + sizeof(long);
-const int ADD_SAMPLING = ADD_SYNC + sizeof(long);
-const int ADD_BEGIN_DATA = ADD_SAMPLING + sizeof(int);
+const int ADD_TIMESTAMP = ADD_BUFFER + sizeof(long);
+const int ADD_SYNC = ADD_TIMESTAMP + sizeof(int);
+const int ADD_NX_SYNC = ADD_SYNC + sizeof(long);
+const int ADD_SAMPLING = ADD_NX_SYNC + sizeof(int);
+const int ADD_NX_SAMPLING = ADD_SAMPLING + sizeof(long);
+const int ADD_BEGIN_DATA = ADD_NX_SAMPLING + sizeof(int);
 const int SIZE_UNIT_DATA = sizeof(int) + sizeof(float) + sizeof(long);
-
-
 
 void reset();
 void setBufferSize(int);
 int getBufferSize();
 int isBufferFull();
-
+void setSync(int);
+void setSampling(int);
+void setNxSampling(long);
+void setNxSync(long);
+void setTimestamp(long);
 
 int pointerData() {
   return ADD_BEGIN_DATA + getBufferSize() * SIZE_UNIT_DATA;
@@ -23,6 +28,12 @@ void reset() {
   for (int i = 0; i < EEPROM_SIZE; i ++){
     EEPROM.write(i, 0xFF);
   }
+  setBufferSize(0);
+  setSync(0);
+  setSampling(0);
+  setNxSampling(0);
+  setNxSync(0);
+  setTimestamp(0);
 }
 
 void flushBuffer() {
@@ -41,7 +52,6 @@ void print_eeprom() {
   Serial.println();
 }
 
-
 void addData(int id, float value, long time){
   if (!isBufferFull()){
     int addr = pointerData();
@@ -58,13 +68,28 @@ void setBufferSize(int size){
   EEPROM.commit();
 }
 
-void setSync(long time){
-  EEPROM.put(ADD_SYNC, time);
+void setSync(int period){
+  EEPROM.put(ADD_SYNC, period);
   EEPROM.commit();
 }
 
-void setSampling(long time){
-  EEPROM.put(ADD_SAMPLING, time);
+void setNxSync(long time){
+  EEPROM.put(ADD_NX_SYNC, time);
+  EEPROM.commit();
+}
+
+void setTimestamp(long timestamp){
+  EEPROM.put(ADD_TIMESTAMP, timestamp);
+  EEPROM.commit();
+}
+
+void setSampling(int period){
+  EEPROM.put(ADD_SAMPLING, period);
+  EEPROM.commit();
+}
+
+void setNxSampling(long time){
+  EEPROM.put(ADD_NX_SAMPLING, time);
   EEPROM.commit();
 }
 
@@ -74,15 +99,33 @@ int getBufferSize(){
   if (i == -1) return 0; else return i;
 }
 
-long getSync(){
-  long i = 0L;
+int getSync(){
+  int i = 0;
   EEPROM.get(ADD_SYNC, i);
+  if (i == -1) return 0; else return i;
+}
+
+long getNxSync(){
+  long i = 0L;
+  EEPROM.get(ADD_NX_SYNC, i);
   if (i == -1) return 0L; else return i;
 }
 
-long getSampling(){
-  long i = 0L;
+int getSampling(){
+  int i = 0;
   EEPROM.get(ADD_SAMPLING, i);
+  if (i == -1) return 0; else return i;
+}
+
+long getNxSampling(){
+  long i = 0L;
+  EEPROM.get(ADD_NX_SAMPLING, i);
+  if (i == -1) return 0L; else return i;
+}
+
+long getTimestamp(){
+  long i = 0L;
+  EEPROM.get(ADD_TIMESTAMP, i);
   if (i == -1) return 0L; else return i;
 }
 
